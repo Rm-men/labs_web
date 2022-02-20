0ddff8e943da1ca4badae76779daf8d764845c63
Romance and Magic <44509725+Rm-men@users.noreply.github.com>
2022-02-20 14:55:31 +0300

    7.3

---
+++ main/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Fio lastName="Заболотский" firstName="Александр" middleName="Николаевич"></Fio>
+    <Vyatsu :results="results"></Vyatsu>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+import Fio from '../../fio/src/components/Fio.vue';
+import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
+
+// const url = 'https://new.vyatsu.ru/';
+const url = 'http://127.0.0.1:5500/main/src/components/8.Html';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Fio,
+    Vyatsu,
+  },
+  data() {
+    return { Fio, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response: any) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error: any) => console.log(error));
+  },
+});
+</script>
