07c2f8be0dd15590b1abcfbdc3b14175a710d183
Romance and Magic <44509725+Rm-men@users.noreply.github.com>
2022-04-29 11:48:36 +0300

    ничего не работает

--- main/src/components/factorial.vue
+++ main/src/components/factorial.vue
@@ -9,11 +9,29 @@
 </template>
 
 <script lang="ts">
+import factorial from "./functions.wasm";
+import * as factorials from "./functions";
 const inputs : HTMLInputElement = document.getElementById('input') as HTMLInputElement;
 const sendBtn : HTMLButtonElement = document.getElementById('send') as HTMLButtonElement;
 const resultText : HTMLSpanElement = document.getElementById('result') as HTMLSpanElement;
+let instance = {
+  ready: new Promise<void>(resolve => {
+    factorial({
+      onRuntimeInitialized() {
+        instance = Object.assign(this, {
+          ready: Promise.resolve()
+      });
+      resolve();
+    }
+  })
+  })
+}
 
 export default {
+  data () {
+    return {result : null}
+  },
+  
   components: {
   },
   props: {
@@ -21,23 +39,35 @@ export default {
   },
   methods: {
     Calculate() {
-      if ('WebAssembly' in window) {
-        console.log('+');
-        //WebAssembly.instantiateStreaming(fetch('functions.wasm')) 
-        fetch("functions.wasm")
-        .then(respoonce)
-          .then((bytes) => {
-            WebAssembly.instantiate(bytes, importObject)
-
-          })
-          .then(result => {
-              const number = inputs.value;
-              const results = result.instance.exports.factorial(number);
-              resultText.innerHTML = results;
-          })
-          .catch(console.error);
-      }
+      instance.ready.then(_ => this.result = instance._factorial(3))
+      // if ('WebAssembly' in window) {
+      //   console.log('+');
+      //   //WebAssembly.instantiateStreaming(fetch('functions.wasm')) 
+      //   fetch("functions.wasm")
+      //   .then(respoonce => respoonce.arrayBuffer())
+      //     .then((bytes) => {
+      //       WebAssembly.instantiate(bytes, importObject)
+      //     })
+      //     .then(result => {
+      //         const number = inputs.value;
+      //         const results = result.instance.exports.factorial(number);
+      //         resultText.innerHTML = results;
+      //     })
+      //     .catch(console.error);
+      // }
     },
   },
 };
 </script>
+
+function importObject(bytes: ArrayBuffer, importObject: any) {
+	throw new Error('Function not implemented.');
+}
+
+function importObject(bytes: ArrayBuffer, importObject: any) {
+	throw new Error('Function not implemented.');
+}
+
+function importObject(bytes: ArrayBuffer, importObject: any) {
+	throw new Error('Function not implemented.');
+}
--- main/webpack.config.js
+++ main/webpack.config.js
@@ -3,31 +3,35 @@ const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.svg$/,
+                use: 'svg-inline-loader',
+            },
+            {
+                test: /\.wasm$/,
+                loaders: ['wasm-loader']
+            },
+        ],
     },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader',
+    plugins: [new VueLoaderPlugin(), new HtmlWebpackPlugin()],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.vue'],
     },
-    {
-      test: /\.svg$/,
-      use: 'svg-inline-loader',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    ],
-  },
-  plugins: [new VueLoaderPlugin(), new HtmlWebpackPlugin()],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.vue'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 };
